2882eb14655e6e228474e536b1def1cbf8988fcf
Ling-Yun Wu <wulingyun@gmail.com>
2016-06-06 10:33:02 +0000

    version 0.3-4

---
+++ src/Corbi.h
@@ -0,0 +1,23 @@
+#include <R.h>
+#include <Rdefines.h>
+#include <R_ext/Rdynload.h>
+#include "misc.h"
+
+/* Interfaces to R */
+
+extern "C" {
+  /* DLL Init */
+  void R_init_Corbi(DllInfo *info);
+
+	/* Utils */
+  SEXP RMultiHyper(SEXP _N, SEXP _M, SEXP _K);
+  SEXP PMultiHyper(SEXP _X, SEXP _K, SEXP _M, SEXP _W);
+  SEXP PMultiNom(SEXP _X, SEXP _K, SEXP _M, SEXP _W);
+
+	SEXP NQ_ShortestDistances(SEXP _Edges, SEXP _Index, SEXP _SourceNodes);
+
+  SEXP PA_Scores(SEXP _D1, SEXP _D2, SEXP _nD1, SEXP _nD2, SEXP _afpLength);
+  
+  SEXP BS_GetSubnets(SEXP _Edges, SEXP _nNodes, SEXP _maxSize);
+  SEXP BS_ExtendSubnets(SEXP _Sub1, SEXP _Sub2, SEXP _size);
+}
